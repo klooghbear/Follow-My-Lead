f93ba98db9d51c5888b6f46daf183a44e09927f5
sebastiankloogh@gmail.com <sebastiankloogh@gmail.com>
2020-03-04 16:05:59 +1300

    add to deploy

--- server/db/ownerData.js
+++ server/db/ownerData.js
@@ -1,24 +1,20 @@
-const knex = require('knex')
-const config = require('../../knexfile')
-const env = process.env.NODE_ENV || 'development'
-const connection = knex(config[env])
+const connection = require('./connection');
 
 function getOwners(db = connection) {
-    return db('owner_table').select()
+  return db('owner_table').select();
 }
-
 
 function getOwner(id, db = connection) {
-    return db('owner_table')
-        .where('id', id)
-        .first()
+  return db('owner_table')
+    .where('id', id)
+    .first();
 }
 
-
 //Add auth shit
-function addOwner(owner, db=connection){
-    return db('owner_table')
-        .insert(owner).debug()
+function addOwner(owner, db = connection) {
+  return db('owner_table')
+    .insert(owner)
+    .debug();
 }
 
 // function updateOwner(id, owner, db = connection) {
@@ -29,23 +25,23 @@ function addOwner(owner, db=connection){
 
 // }
 
-function editOwner(id, owner, db= connection){
-    console.log('db function working')
-    return db('owner_table')
+function editOwner(id, owner, db = connection) {
+  console.log('db function working');
+  return db('owner_table')
     .where('id', id)
     .update({
-        first_name: owner.first_name,
-        last_name: owner.last_name,
-        email: owner.email,
-        photo: owner.photo,
-        location: owner.location     
+      first_name: owner.first_name,
+      last_name: owner.last_name,
+      email: owner.email,
+      photo: owner.photo,
+      location: owner.location
     })
-    .then()
+    .then();
 }
 
 module.exports = {
-    getOwners,
-    getOwner,
-    addOwner,
-    editOwner
-}
+  getOwners,
+  getOwner,
+  addOwner,
+  editOwner
+};
